cd4db83594280d96dcc27e867fa384a9ae9ac3b8
Lionel Tay <lionel.lt.tay@gmail.com>
2019-01-17 14:06:11 +1100

    Add api for magic 8 ball answers

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,33 @@
-console.log('server')
+import express = require("express")
+
+const app = express()
+
+const ANSWERS = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+]
+
+app.get("/answer", (req, res) => {
+  res.send({ answer: ANSWERS[Math.floor(Math.random() * ANSWERS.length)] })
+})
+
+const PORT = 3030
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`))
